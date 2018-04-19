760809ba5fa57ca1dd0e7f7c7931e75c2d51b61f
Adam <adam.zouari@heig-vd.ch>
2018-04-19 23:39:19 +0200

    retrieve the info in the .properties file

--- labo03-SMTP/src/main/java/com/mycompany/labo03/mailrobot/config/ConfigurationManager.java
+++ labo03-SMTP/src/main/java/com/mycompany/labo03/mailrobot/config/ConfigurationManager.java
@@ -1,11 +1,42 @@
-
-
 package com.mycompany.labo03.mailrobot.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  *
  * @author Adam Zouari
+ * @author Walid Koubaa
  */
 public class ConfigurationManager {
+    
+    Properties properties = new Properties();
+    InputStream file;
 
+    public ConfigurationManager() throws IOException{
+        file = new FileInputStream("config.propreties");
+        properties.load(file);
+    }
+    
+    public String getSmtpServerAdress()
+    {
+        return properties.getProperty("smtpServerAdress");
+    }
+    
+    public int getSmtpServerPort()
+    {
+        return Integer.valueOf(properties.getProperty("smtpServerPort"));
+    }
+                
+    public int getNumberOfGroups()
+    {
+        return Integer.valueOf(properties.getProperty("numberOfGroups"));
+    }
+    
+    public int getNumberOfPersonPerGroup()
+    {
+        return Integer.valueOf(properties.getProperty("numberOfPersonPerGroup"));
+    }
 }
--- labo03-SMTP/src/main/java/com/mycompany/labo03/mailrobot/config/IConfigurationManager.java
+++ labo03-SMTP/src/main/java/com/mycompany/labo03/mailrobot/config/IConfigurationManager.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.labo03.mailrobot.config;
 
 /**
  *
  * @author Adam Zouari
+ * @author Walid Koubaa
  */
 public interface IConfigurationManager {
-    
+
+    public String getSmtpServerAdress();
+
+    public int getSmtpPort();
+
+    public int getNumberOfGroups();
+
+    public int getNumberOfPersonPerGroup();
+
 }
