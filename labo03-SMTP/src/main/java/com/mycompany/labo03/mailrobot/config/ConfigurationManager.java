d4b8ce61c808107e197de8df83ab939791a4315a
Adam <adam.zouari@heig-vd.ch>
2018-04-20 02:21:27 +0200

    update config manager

--- labo03-SMTP/src/main/java/com/mycompany/labo03/mailrobot/config/ConfigurationManager.java
+++ labo03-SMTP/src/main/java/com/mycompany/labo03/mailrobot/config/ConfigurationManager.java
@@ -1,8 +1,15 @@
 package com.mycompany.labo03.mailrobot.config;
 
+import com.mycompany.labo03.mailrobot.model.mail.Message;
+import com.mycompany.labo03.mailrobot.model.mail.Person;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -11,32 +18,65 @@ import java.util.Properties;
  * @author Walid Koubaa
  */
 public class ConfigurationManager {
-    
-    Properties properties = new Properties();
-    InputStream file;
 
-    public ConfigurationManager() throws IOException{
-        file = new FileInputStream("config.propreties");
-        properties.load(file);
+    private Properties confProperties;
+    private final FileInputStream confPropertiesFile;
+    private final BufferedReader victimsFile, messagesFile;
+    private static final String MAIL_SEPARATOR = "===";
+    private List<String> messages;
+    private List<Person> victims;
+
+
+
+    public ConfigurationManager() throws IOException {
+        confPropertiesFile = new FileInputStream("config.propreties");
+        confProperties.load(confPropertiesFile);
+
+        victimsFile = new BufferedReader(new InputStreamReader(new FileInputStream("victims.utf8"), "UTF-8"));
+        messagesFile = new BufferedReader(new InputStreamReader(new FileInputStream("messages.utf8"), "UTF-8"));
+
+        messages = new ArrayList<>();
+        victims = new ArrayList<>();
+    }
+
+    public String getSmtpServerAddress() {
+        return confProperties.getProperty("smtpServerAddress");
+    }
+
+    public int getSmtpServerPort() {
+        return Integer.valueOf(confProperties.getProperty("smtpServerPort"));
+    }
+
+    public int getNumberOfGroups() {
+        return Integer.valueOf(confProperties.getProperty("numberOfGroups"));
+    }
+
+    public int getNumberOfPersonPerGroup() {
+        return Integer.valueOf(confProperties.getProperty("numberOfPersonPerGroup"));
+    }
+
+    public void loadVictims() throws IOException
+    {
+        String address;
+        while((address = victimsFile.readLine()) != null )
+        {
+            victims.add(new Person(address));
+        }
     }
     
-    public String getSmtpServerAdress()
+    public void loadMessages() throws IOException
     {
-        return properties.getProperty("smtpServerAdress");
+        String message = "", line;
+        while((line = messagesFile.readLine()) != null )
+        {
+            if(!line.equals(MAIL_SEPARATOR))
+                message += line + SmtpProtocol.EOL;
+            else
+                messages.add(message);
+        }
     }
     
-    public int getSmtpServerPort()
-    {
-        return Integer.valueOf(properties.getProperty("smtpServerPort"));
-    }
-                
-    public int getNumberOfGroups()
-    {
-        return Integer.valueOf(properties.getProperty("numberOfGroups"));
-    }
-    
-    public int getNumberOfPersonPerGroup()
-    {
-        return Integer.valueOf(properties.getProperty("numberOfPersonPerGroup"));
-    }
+    public List<Person> getVictims(){return victims;}
+    public List<String> getMessages(){return messages;}
+
 }
